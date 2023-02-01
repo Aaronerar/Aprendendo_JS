61658ac47ec48fd72165ae5037b135e253767daf
Aaronerar <aaronnader@hotmail.com>
2023-02-01 14:20:15 -0300

    Estou mostrand meus scripts.

---
+++ index.js
@@ -0,0 +1,12 @@
+let nome = "José";
+let sobreNome = "Ribeiro";
+let idade = 27;
+const numCliente = 190867;
+let ehQualificado = "true";
+let numAnos = 6;
+let valorEmprestimo = 200000;
+let taxaDeJuros = 12;
+let juros = valorEmprestimo * taxaDeJuros * numAnos;
+let montante = valorEmprestimo + juros;
+
+console.log("O valor do montante é: " + montante);
